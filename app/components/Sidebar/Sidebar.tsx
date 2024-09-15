365c4bbba7fd8e5bf1c92342e43561afc9b39a44
xfreshu <lprzybyszcontact@gmail.com>
2024-09-15 16:16:22 +0200

    Working on sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -24,14 +24,12 @@ const ResponsiveSidebar = () => {
         <HamburgerButton onClick={toggleSidebar}>
           <FaBars size={30} color="#333" />
         </HamburgerButton>
-
+        //TODO
         <SidebarContainer isOpen={isOpen}>
           <ActiveLink href="/profil">Mój profil</ActiveLink>
           <ActiveLink href="/dashboard">Dashboard</ActiveLink>
         </SidebarContainer>
-
         <Overlay isOpen={isOpen} onClick={toggleSidebar} />
-
         <Content>
           <h1>Page Content</h1>
           <p>This is where your main content will be displayed.</p>
